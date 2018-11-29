dd3bcef988f63c40bd09274b57b6dea00660442b
Chad Hegerhorst <chad.hegerhorst@vivint.com>
2018-11-29 14:07:37 -0700

    added a few more comments

--- Tests/test_all_fields_required.rb
+++ Tests/test_all_fields_required.rb
@@ -7,6 +7,8 @@ class LoginClass < FaceBookTestBase
 
 
   def test_no_fields_entered
+    # Verify that if no info is entered into any of the fields and the submit button is clicked, all
+    # fields change to an error state.
     login_page.click_submit
 
     assert_equal(true , login_page.first_name_in_error_state)
@@ -16,6 +18,8 @@ class LoginClass < FaceBookTestBase
   end
 
   def test_firstname_entered
+    # Verify that if only the fires name is entered and the submit button is clicked, all fields except
+    # for the first name filed is in an error state.
     login_page.set_first_name("Mike")
     login_page.click_submit
 
@@ -26,6 +30,8 @@ class LoginClass < FaceBookTestBase
   end
 
   def test_lastname_entered
+    # Verify that if only the last name is entered and the submit button is clicked, all fields except
+    # for the last name field is in an error state.
     login_page.set_last_name("McTester")
     login_page.click_submit
 
@@ -36,6 +42,8 @@ class LoginClass < FaceBookTestBase
   end
 
   def test_email_entered
+    # Verify that if only the email is entered and the submit button is clicked, all fields except
+    # for the email field is in an error state.
     login_page.set_email("mike.mctester@test.com")
     login_page.click_submit
 
@@ -46,6 +54,8 @@ class LoginClass < FaceBookTestBase
   end
 
   def test_password_entered
+    # Verify that if only the password is entered and the submit button is clicked, all fields except
+    # for the password field is in an error state.
     login_page.set_passwd("12345")
     login_page.click_submit
 
